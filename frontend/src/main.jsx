fa8e13daf8e75f40b3ba8ca8cb5f739437a5ddce
nic627 <157721410+nic627@users.noreply.github.com>
2025-10-18 20:19:39 -0400

    putting it in the main app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-
-// 👇 import your test component
-import App from './App.jsx'
-
-
-// 👇 render your test page instead of App
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
